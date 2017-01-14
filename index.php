7bafa6fac58ef0b428e90f6610bfc060cc5506f3
XXalice <kefuji555@gmail.com>
2017-01-15 03:36:09 +0900

    commit

--- index.php
+++ index.php
@@ -1,34 +1,35 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+  require_once __DIR__ . '/vendor/autoload.php';
+  require_once __DIR__ . '/searchImage/config.php';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+  $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelToken);
+  $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-try {
-  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-  error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
-  error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
-} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
-  error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
-} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-  error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
-}
-
-foreach ($events as $event) {
-  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
-    error_log('Non message event has come');
-    continue;
+  $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+  try {
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+  } catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+    error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
+  } catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+    error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
+  } catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+    error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
+  } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+    error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
   }
-  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-    error_log('Non text message has come');
-    continue;
+
+  foreach ($events as $event) {
+    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+      error_log('Non message event has come');
+      continue;
+    }
+    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+      error_log('Non text message has come');
+      continue;
+    }
+    $bot->replyText($event->getReplyToken(), $event->getText());
   }
-  $bot->replyText($event->getReplyToken(), $event->getText());
-}
 
   echo "ok";
 
---
+++ searchImage/config.php
@@ -0,0 +1,6 @@
+<?php
+
+    $channelToken = getenv('CHANNEL_ACCESS_TOKEN');
+    $channelSecret = getenv('CHANNEL_SECRET');
+
+?>
