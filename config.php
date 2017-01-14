36634e2fa9020cbac15a6d3968ac88d874b6f21a
XXalice <kefuji555@gmail.com>
2017-01-15 03:27:50 +0900

    commit

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $channelToken = getenv('CHANNEL_ACCESS_TOKEN');
-    $channelSecret = getenv('CHANNEL_SECRET');
-
-?>
--- index.php
+++ index.php
@@ -1,53 +1,33 @@
 <?php
 
-    use LINE\LINEBot;
-    use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-    use LINE\LINEBot\Constant\HTTPHeader;
-    use LINE\LINEBot\Exception\InvalidSignatureException;
-    use LINE\LINEBot\Exception\UnknownEventTypeException;
-    use LINE\LINEBot\Exception\UnknownMessageTypeException;
-    use LINE\LINEBot\Exception\InvalidEventRequestException;
+require_once __DIR__ . '/vendor/autoload.php';
 
-    //SDKの読み込み
-    require_once __DIR__ . '/vendor/autoload.php';
-    require_once 'config.php';
-    require_once 'searchImage.php';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
-    //認証
-    $bot = new LINEBot(CurlHTTPClient($channelToken),[
-        'channelSecret'=>$channelSecret
-    ]);
-    $app = new searchImage();
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
+}
 
-    //jsonファイルからイベントの所得及びエラー処理
-    $signature = $_SERVER["HTTP_" . HTTPHeader::LINE_SIGNATURE];
-    try {
-    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-    } catch(InvalidSignatureException $e) {
-        error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
-    } catch(UnknownEventTypeException $e) {
-        error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
-    } catch(UnknownMessageTypeException $e) {
-        error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
-    } catch(InvalidEventRequestException $e) {
-        error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
-    }
-    
-    foreach($events as $event){
-        if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
-            error_log('Non message event has come');
-            continue;
-        }
-        if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-            error_log('Non text message has come');
-            continue;
-        }
-        //アプリケーション実行
-        $app->search($event->getText());
-        //アプリケーションから帰ってきたものを送りつける
-        $bot->replyText($event->getReplyToken(),$app);
-    }
+foreach ($events as $event) {
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+    error_log('Non message event has come');
+    continue;
+  }
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+    error_log('Non text message has come');
+    continue;
+  }
+  $bot->replyText($event->getReplyToken(), $event->getText());
+}
 
-    echo 'ok';
-    
-?>
+ ?>
