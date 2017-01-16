5df479e42ac1ed26a0592959e742efee89ac7304
XXalice <kefuji555@gmail.com>
2017-01-16 15:57:30 +0900

    commit

--- searchImage/searchImage.php
+++ searchImage/searchImage.php
@@ -2,20 +2,22 @@
 
     class searchImage{
 
+        //画像検索関数
+        //返り値$img　[0]=>サムネURL
+        //           [1]=>画像本体URL
         public function search($str){    
             global $flickr;
             $option = [
                 'text'=>$str,
                 'media'=>'photos',
-                'per_page'=>5,
+                'per_page'=>20,
                 'extras'=>'url_q,url_m',
                 'sort'=>'interestingness-desc'
             ];
             $img = [];
             $obj = json_decode(json_encode($flickr->photos_search($option)));
-            $photo = $obj->photo[mt_rand(0,4)];
-                $img[0] = $photo->url_q;
-                $img[1] = $photo->url_m;
+            $photo = $obj->photo[mt_rand(0,count($obj->photo))];
+            array_push($img,$photo->url_q,$photo->url_m);
             return $img;            
         }  
     }
