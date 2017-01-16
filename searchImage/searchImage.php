b10e66d35cf38a9a983edd56cf2d85abeb50a8dc
XXalice <kefuji555@gmail.com>
2017-01-16 15:49:14 +0900

    commit

--- searchImage/searchImage.php
+++ searchImage/searchImage.php
@@ -7,16 +7,15 @@
             $option = [
                 'text'=>$str,
                 'media'=>'photos',
-                'per_page'=>1,
+                'per_page'=>5,
                 'extras'=>'url_q,url_m',
                 'sort'=>'interestingness-desc'
             ];
             $img = [];
             $obj = json_decode(json_encode($flickr->photos_search($option)));
-            foreach($obj->photo as $photo){
+            $photo = $obj->photo[mt_rand(0,4)]
                 $img[0] = $photo->url_q;
                 $img[1] = $photo->url_m;
-            }
             return $img;            
         }  
     }
